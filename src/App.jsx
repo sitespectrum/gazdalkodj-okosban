e66c15ca5112009cf638bf67633c12489a617485
akosfodor <akosfodor10@gmail.com>
2025-01-14 16:19:21 +0100

    board is done

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './Fields.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='fields'>
+      <img src="./src/Pictures/Start.jpg" alt="Start" className='start'/>
+      <img src="./src/Pictures/1. Mező.png" alt="1. Mező" className='first'/>
+      <img src="./src/Pictures/2. Mező.png" alt="2. Mező" className='second'/>
+      <img src="./src/Pictures/3. Mező.png" alt="3. Mező" className='third'/>
+      <img src="./src/Pictures/4. Mező.png" alt="4. Mező" className='fourth'/>
+      <img src="./src/Pictures/5. Mező.png" alt="5. Mező" className='fifth'/>
+      <img src="./src/Pictures/6. Mező.png" alt="6. Mező" className='sixth'/>
+      <img src="./src/Pictures/7. Mező.png" alt="7. Mező" className='seventh'/>
+      <img src="./src/Pictures/8. Mező.png" alt="8. Mező" className='eighth'/>
+      <img src="./src/Pictures/9. Mező.png" alt="9. Mező" className='ninth'/>
+      <img src="./src/Pictures/10. Mező.png" alt="10. Mező" className='tenth'/>
+      <img src="./src/Pictures/11. Mező.png" alt="11. Mező" className='eleventh'/>
+      <img src="./src/Pictures/12. Mező.png" alt="12. Mező" className='twelfth'/>
+      <img src="./src/Pictures/13. Mező.jpg" alt="13. Mező" className='thirteenth'/>
+      <img src="./src/Pictures/14. Mező (kisebb).png" alt="14. Mező" className='fourteenth'/>
+      <img src="./src/Pictures/15. Mező.png" alt="15. Mező" className='fifteenth'/>
+      <img src="./src/Pictures/16. Mező.png" alt="16. Mező" className='sixteenth'/>
+      <img src="./src/Pictures/17. Mező.png" alt="17. Mező" className='seventeenth'/>
+      <img src="./src/Pictures/18. Mező.png" alt="18. Mező" className='eighteenth'/>
+      <img src="./src/Pictures/19. Mező.png" alt="19. Mező" className='nineteenth'/>
+      <img src="./src/Pictures/20. Mező.png" alt="20. Mező" className='twenteenth'/>
+      <img src="./src/Pictures/21. Mező.png" alt="21. Mező" className='twenty-first'/>
+      <img src="./src/Pictures/22. Mező 1.png" alt="22. Mező" className='twenty-second'/>
+      <img src="./src/Pictures/23. Mező.png" alt="23. Mező" className='twenty-third'/>
+      <img src="./src/Pictures/24. Mező.png" alt="24. Mező" className='twenty-fourth'/>
+      <img src="./src/Pictures/25. Mező.png" alt="25. Mező" className='twenty-fifth'/>
+      <img src="./src/Pictures/26. Mező.png" alt="26. Mező" className='twenty-sixth'/>
+      <img src="./src/Pictures/27. Mező.png" alt="27. Mező" className='twenty-seventh'/>
+    </div>
+    </>
+  )
+}
+
+export default App
