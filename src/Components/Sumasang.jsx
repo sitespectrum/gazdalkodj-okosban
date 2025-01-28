eb1e9fbe9bc72084f7a2591a21e1c300b5ed59e6
akosfodor <akosfodor10@gmail.com>
2025-01-28 08:51:11 +0100

    more changes

--- src/Components/Sumasang.jsx
+++ src/Components/Sumasang.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Sumasang = ({ onClose, currentPlayer, reducePlayerMoney }) => {
     return (
         <div className='sumasang'>
-            <p>Sumasang 65" TV - 599 990 Ft <button className='buyButton' onClick={() => reducePlayerMoney(currentPlayer, 599990)}>Vásárlás</button></p>
+            <p>Sumasang 65" QLED 8K TV - 599 990 Ft <button className='buyButton' onClick={() => reducePlayerMoney(currentPlayer, 599990)}>Vásárlás</button></p>
             <button className='sumasang-close' onClick={onClose}>Bezárás</button>
         </div>
     );
