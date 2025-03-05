2f51aa98436b43948efc2511644ca5bd28ac12fc
SledDev <sleddev@asd.dog>
2025-03-05 01:42:16 +0100

    adjusted puppet positions

--- src/lib/fields-config.jsx
+++ src/lib/fields-config.jsx
@@ -13,8 +13,8 @@ export const FIELDS = [
   {
     id: 0,
     name: "Start",
-    x: 88.9,
-    y: 80,
+    x: 92.45,
+    y: 83,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].money += 170_000;
@@ -27,15 +27,15 @@ export const FIELDS = [
   {
     id: 1,
     name: "Lucky 1",
-    x: 71.5,
-    y: 80,
+    x: 73.7,
+    y: 84,
     action: ({ openPopup }) => openPopup("lucky", <Lucky />),
   },
   {
     id: 2,
     name: "Trash",
-    x: 64,
-    y: 80,
+    x: 65.4,
+    y: 84,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].money -= 1500;
@@ -47,37 +47,37 @@ export const FIELDS = [
   {
     id: 3,
     name: "Elza",
-    x: 56.5,
-    y: 80,
+    x: 57.1,
+    y: 84,
     action: ({ openPopup }) => openPopup("elza", <Elza />),
   },
-  { id: 4, name: "South Station", x: 48.9, y: 80, isStop: true },
+  { id: 4, name: "South Station", x: 48.9, y: 84, isStop: true },
   {
     id: 5,
     name: "Bank Robbery",
-    x: 41.3,
-    y: 80,
+    x: 40.6,
+    y: 84,
     action: ({ openPopup }) => openPopup("bankrobbery", <BankRobbery />),
   },
   {
     id: 6,
     name: "Elza and Idea",
-    x: 33.9,
-    y: 80,
+    x: 32.5,
+    y: 84,
     action: ({ openPopup }) => openPopup("elzaandidea", <ElzaAndIdea />),
   },
   {
     id: 7,
     name: "Lucky 2",
-    x: 26.4,
-    y: 80,
+    x: 24.2,
+    y: 84,
     action: ({ openPopup }) => openPopup("lucky", <Lucky />),
   },
   {
     id: 8,
     name: "Smoking",
-    x: 18.9,
-    y: 80,
+    x: 16,
+    y: 84,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].money -= 1500;
@@ -92,8 +92,8 @@ export const FIELDS = [
   {
     id: 10,
     name: "Movie Theater",
-    x: 8,
-    y: 63,
+    x: 5,
+    y: 64,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].money -= 5000;
@@ -102,14 +102,14 @@ export const FIELDS = [
         };
       }),
   },
-  { id: 11, name: "West Station", x: 8, y: 45, isStop: true },
-  { id: 12, name: "Casino", x: 8, y: 28 },
+  { id: 11, name: "West Station", x: 5, y: 45, isStop: true },
+  { id: 12, name: "Casino", x: 5, y: 26.5 },
 
   {
     id: 13,
     name: "Airport",
-    x: 8,
-    y: 12,
+    x: 5,
+    y: 10,
     action: async (props) => {
       await new Promise((resolve) => setTimeout(resolve, 1000));
       props.updateGameState(
@@ -133,22 +133,22 @@ export const FIELDS = [
   {
     id: 14,
     name: "Bob the Builder",
-    x: 18.9,
-    y: 12,
+    x: 16,
+    y: 7.5,
     action: ({ openPopup }) => openPopup("bobthebuilder", <Bobthebuilder />),
   },
   {
     id: 15,
     name: "Car Shop",
-    x: 26.4,
-    y: 12,
+    x: 24.2,
+    y: 7.5,
     action: ({ openPopup }) => openPopup("carshop", <Carshop />),
   },
   {
     id: 16,
     name: "Car travel",
-    x: 33.9,
-    y: 12,
+    x: 32.5,
+    y: 7.5,
     action: async (props) => {
       if (props.gameState.players[props.playerIndex].hasCar) {
         await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -171,16 +171,16 @@ export const FIELDS = [
   {
     id: 17,
     name: "Lucky 3",
-    x: 41.3,
-    y: 12,
+    x: 40.6,
+    y: 7.5,
     action: ({ openPopup }) => openPopup("lucky", <Lucky />),
   },
-  { id: 18, name: "North Station", x: 48.9, y: 12, isStop: true },
+  { id: 18, name: "North Station", x: 48.9, y: 7.5, isStop: true },
   {
     id: 19,
     name: "Abidas",
-    x: 56.5,
-    y: 12,
+    x: 57.1,
+    y: 7.5,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].money -= 15_000;
@@ -192,16 +192,16 @@ export const FIELDS = [
   {
     id: 20,
     name: "Idea",
-    x: 64,
-    y: 12,
+    x: 65.4,
+    y: 7.5,
     action: ({ openPopup }) => openPopup("idea", <Idea />),
   },
-  { id: 21, name: "Bank 4", x: 71.5, y: 12 },
+  { id: 21, name: "Bank 4", x: 73.7, y: 7.5 },
   {
     id: 22,
     name: "ABC",
-    x: 79.2,
-    y: 12,
+    x: 81.8,
+    y: 7.5,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].money -= 10_000;
@@ -213,8 +213,8 @@ export const FIELDS = [
   {
     id: 23,
     name: "Hospital",
-    x: 88.9,
-    y: 12,
+    x: 94,
+    y: 11,
     action: ({ updateGameState, playerIndex }) => {
       updateGameState((prevGameState) => {
         prevGameState.players[playerIndex].inHospital = "arrived";
@@ -225,12 +225,12 @@ export const FIELDS = [
       });
     },
   },
-  { id: 24, name: "Insurance", x: 88.9, y: 28 },
-  { id: 25, name: "East Station", x: 88.9, y: 45, isStop: true },
+  { id: 24, name: "Insurance", x: 93, y: 26 },
+  { id: 25, name: "East Station", x: 93, y: 45, isStop: true },
   {
     id: 26,
     name: "Roll again",
-    x: 88.9,
+    x: 93,
     y: 64,
     action: ({ updateGameState, playerIndex }) =>
       updateGameState((prevGameState) => {
@@ -241,5 +241,5 @@ export const FIELDS = [
       }),
   },
 
-  { id: 27, name: "Jail (locked up)", x: 10, y: 80 },
+  { id: 27, name: "Jail (locked up)", x: 5, y: 84 },
 ];
