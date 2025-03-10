2e4ed14074fa87236a8bd7ea5a4ee6fcbf2175ca
SledDev <sleddev@asd.dog>
2025-03-10 18:42:56 +0100

    copy dist/client contents to dist

--- react-router.config.js
+++ react-router.config.js
@@ -5,4 +5,24 @@ export default {
   appDirectory: "src",
   ssr: false,
   buildDirectory: "dist",
+  buildEnd: async () => {
+    const fs = require("fs").promises;
+    const path = require("path");
+
+    const clientDir = path.join("dist", "client");
+    const distDir = "dist";
+
+    const files = await fs.readdir(clientDir);
+    await Promise.all(
+      files.map(async (file) => {
+        const srcPath = path.join(clientDir, file);
+        const destPath = path.join(distDir, file);
+        await fs.rename(srcPath, destPath);
+      })
+    );
+
+    await fs.rmdir(clientDir);
+
+    console.log("Copied client files to dist");
+  },
 };
